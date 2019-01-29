9fcb49bee207e0fb99b7a71ba4888d1dc71b8138
Rob Stryker <rob@oxbeef.net>
2019-01-29 10:44:20 -0500

    Initial commit
    Signed-off-by: Rob Stryker <rob@oxbeef.net>

---
+++ src/main/java/net/oxbeef/rsp/server/test/App.java
@@ -0,0 +1,13 @@
+package net.oxbeef.rsp.server.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
